86e58ae82c53f9f7dadbd0d46a43a14dd6077802
anna.deineko <anna.deineko@student.kdg.be>
2024-01-25 03:23:48 +0100

    init

---
+++ src/main/java/com/spring/todoapplication/TodoapplicationApplication.java
@@ -0,0 +1,13 @@
+package com.spring.todoapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoapplicationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoapplicationApplication.class, args);
+	}
+
+}
